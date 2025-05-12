57367a5a77e6fd4fdc8a1a2a5b877d3b53a7b8cd
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-12 16:29:34 +0200

    feat: add routes with MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import {BrowserRouter, Routes, Route } from 'react-router';
+
+import MainLayout from './assets/layouts/MainLayout';
+import HomePage from './assets/pages/HomePage';
+
 export default function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path='/' element={<HomePage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
