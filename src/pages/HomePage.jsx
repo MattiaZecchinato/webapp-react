6c41b7ae5dd882f500641c93c3b88b15ffad19e5
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-12 21:42:43 +0200

    feat: add context for movie

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
-import moviesList from '../data/moviesDemo.json';
+import { useContext } from 'react';
+
+//context
+import MovieContext from '../contexts/MovieContext';
+//components
 import MovieCard from '../components/MovieCard';
 
 export default function HomePage() {
 
+    const moviesList = useContext(MovieContext);
+
     return <>
         <h1>Movies List</h1>
         <div className='movies-box'>
-            {moviesList.map(movie => <MovieCard id={movie.id} title={movie.title} director={movie.director} />)}
+            {moviesList.map(movie => <MovieCard key={movie.id} id={movie.id} title={movie.title} director={movie.director} year={movie.release_year} />)}
         </div>
     </>
 }
