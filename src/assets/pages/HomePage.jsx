e737da7a096124f4c85f6c6510d2461b84741b14
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-12 17:54:22 +0200

    feat: create HomePage

---
+++ src/assets/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import moviesList from '../../data/moviesDemo.json';
+import MovieCard from '../components/MovieCard';
+
+export default function HomePage() {
+
+    return <>
+        <h1>Movies List</h1>
+        {moviesList.map(movie => <MovieCard id={movie.id} title={movie.title} director={movie.director} />)}
+    </>
+}
