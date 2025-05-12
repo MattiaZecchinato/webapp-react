8b95b8649f248c9360b5c3b47cc2790caf9cd7cf
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-12 21:44:49 +0200

    feat: create the page for the details of the selected movie

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,34 @@
 import { useParams } from "react-router";
+import { useContext } from "react";
+
+//context
+import MovieContext from "../contexts/MovieContext";
 
 export default function MovieDetails() {
 
         const { id } = useParams();
 
-        console.log(`use params con id:${id}`)
+        const movies = useContext(MovieContext);
+
+        const currentMovie = movies.find(movie => movie.id === parseInt(id));
+
+        console.log(currentMovie);
 
     return <>
-        <p>id:{id}</p>
+        <div className="box-det">
+            <h1>{currentMovie.title}</h1>
+            <div className="movie-box-det">
+                <div className="img-box-det">
+                    <img src={currentMovie.imagePath} alt={currentMovie.title} />
+                </div>
+                <div className="details-box-det">
+                    <p>Director: {currentMovie.director}</p>
+                    <p>Genre: {currentMovie.genre}</p>
+                    <p>Year: {currentMovie.release_year}</p>
+                    <p>Description: {currentMovie.abstract}</p>
+                    <p>Average vote: {currentMovie.avg_vote}</p>
+                </div>
+            </div>
+        </div>
     </>
 }
