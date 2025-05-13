628ec1b433de5d530581a4de239bb9e04d1bc60e
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-13 14:07:58 +0200

    feat: add personal css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router";
 import NavBar from "./NavBar";
 
+import style from '../components-style/Header.module.css';
+
 export default function Header() {
 
-    return <header>
+    return <div className={style.headerBox}>
             <NavLink to='/'><img src="logo.jpg" alt="logo" /></NavLink>
             <NavBar />
-    </header>
+    </div>
 }
