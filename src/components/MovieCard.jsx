520022dc8bdab882b2426fe727960cc974af97b2
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-14 12:36:31 +0200

    feat: change props and add new field

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,19 +2,20 @@ import { NavLink } from "react-router";
 
 import style from '../components-style/MovieCard.module.css';
 
-export default function MovieCard({ id, title, director, year, image }) {
+export default function MovieCard({ movie }) {
 
-    const linkMovieDetails = `/${id}`;
+    const linkMovieDetails = `/${movie.id}`;
 
     return <>
         <div className={style.card}>
             <div className={style.imgBox}>
-                <img src={image} alt={title} className={style.img} />
+                <img src={movie.imagePath} alt={movie.title} className={style.img} />
             </div>
             <div className={style.detailsBox}>
-                <p>{title}</p>
-                <p>{director}</p>
-                <p>{year}</p>
+                <h3>{movie.title}</h3>
+                <p>Author: <span>{movie.director}</span></p>
+                <p>Reviews average vote: {movie.avg_vote}</p>
+                <p>{movie.abstract}</p>
                 <NavLink to={linkMovieDetails}>See More</NavLink>
             </div>
         </div>
